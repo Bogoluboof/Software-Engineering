99c27c28cba76705897d06c4bc265b9ed5f35023
Aleksey Tryasolobov <80001721+Bogoluboof@users.noreply.github.com>
2021-06-28 02:35:06 +0500

    Delete 3.js

--- labrab03/3.js
+++
@@ -1,8 +0,0 @@
-const _ = require('lodash');
-const { readCsv } = require('./csv');
-const Adult = (students) => {
-    return _.filter(students, student => student.age >= 18);
-}
-
-let students = readCsv('students');
-console.log(Adult(students));
